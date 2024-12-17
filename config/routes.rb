01dd1f8e717b37ac77ad5983400bf9331922030a
OliverGreenaway <oliver.greenaway@sharesight.co.nz>
2024-12-18 10:58:05 +1300

    Add Devise and setup for API auth

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root "home#index"
-
   namespace :api do
     resources :tournaments, only: [:index]
   end
+
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout'
+  },
+  controllers: {
+    sessions: 'users/sessions'
+  }
 end
