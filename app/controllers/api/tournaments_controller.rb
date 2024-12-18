df8fb2d9945c03d8a4925a5d979074bc1ab37f29
Oliver Greenaway <oliver.greenaway@sharesight.co.nz>
2024-12-19 10:52:33 +1300

    Small improvements (#7)
    * Order tournaments list
    
    * Remove tournament shows from admin controller and add show tournament to public endpoint

--- app/controllers/api/tournaments_controller.rb
+++ app/controllers/api/tournaments_controller.rb
@@ -1,9 +1,13 @@
 class Api::TournamentsController < Api::BaseController
 
   def index
-    tournaments = Tournament.all
-
+    tournaments = Tournament.all.order(&:starts_at).reverse
     render json: {tournaments: tournaments.collect(&:to_json)}
+  end
+
+  def show
+    tournament = Tournament.find(params[:id])
+    render json: { tournament: tournament.to_json }
   end
 
 end
