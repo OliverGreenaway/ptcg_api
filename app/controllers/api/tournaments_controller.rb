c30c8d383ed53ade18dd5a6f5ef6a108cabab736
OliverGreenaway <oliver.greenaway@sharesight.co.nz>
2024-12-19 10:49:14 +1300

    Order tournaments list

--- app/controllers/api/tournaments_controller.rb
+++ app/controllers/api/tournaments_controller.rb
@@ -1,7 +1,7 @@
 class Api::TournamentsController < Api::BaseController
 
   def index
-    tournaments = Tournament.all
+    tournaments = Tournament.all.order(&:starts_at).reverse
 
     render json: {tournaments: tournaments.collect(&:to_json)}
   end
