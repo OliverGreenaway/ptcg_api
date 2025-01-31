a95185e008b4467cf60720f9e67b13da546babe5
OliverGreenaway <oliver.greenaway@sharesight.co.nz>
2025-01-31 15:30:51 +1300

    Add logic to work out the status of a tournament

--- app/models/data_providers/arcanine.rb
+++ app/models/data_providers/arcanine.rb
@@ -10,6 +10,14 @@ class DataProviders::Arcanine
       "#{BASE_URL}/tournament/#{identifier}"
     end
 
+    def roster_url(identifier)
+      "#{BASE_URL}/roster/#{identifier}"
+    end
+
+    def pairings_url(identifier)
+      "#{BASE_URL}/pairings/#{identifier}"
+    end
+
     def provider_id
       "RK9"
     end
